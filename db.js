7e58f402d59345dc115fab8e88a5bc0d844cb894
nairboon <remo.hertig@bluewin.ch>
2011-04-21 01:04:47 +0200

    basic structure, dataprovider interface

--- db.js
+++ db.js
@@ -1,28 +1,39 @@
 
-require('joose');
+
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema, ObjectId = Schema.ObjectId;
 
-var user = new Schema({
+var UserSchema = new Schema({
 	id: ObjectId,
 	username: String,
 	password: String,
 	registered: Date
 });
 
-var data = new Schema({
+var DataSchema = new Schema({
 	type: String,
 	owner: String,
 	content: String
 });
+mongoose.model('users',UserSchema);
+mongoose.model('data',DataSchema);
 
-Class('DataProvider',{
-	methods: {
-		authorizeUser: function(user) {},
-		getData: function(id,rev) {},
-		setData: function(id,newcontent) {}
-		 }
-});
+var DataProvider = function(){
+		User = mongoose.model('users');
+		Data  = mongoose.model('data');
+		
+		this.authorizeUser = function(user) {}
+		this.getData = function(id,rev) { return {msg: "no data for" + id}; }
+		this.setData = function(id,newcontent) {}
+		this.addData = function(param) {
+			var ins = new Data();
+			ins.type = param.type;
+			ins.owner = param.owner;
+			ins.content = param.content;
+			ins.save();
+		}
+		 
+};
 
 
 exports.DataProvider = DataProvider;
