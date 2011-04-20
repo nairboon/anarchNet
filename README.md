b774a7a2effecf9b4b6c29f466af90330a0b320c
nairboon <remo.hertig@bluewin.ch>
2011-04-20 13:49:04 +0200

    first commit

--- README.md
+++
@@ -1,3 +0,0 @@
-# Parannus
- * is a experimental framework for collaborative data editing and presentation
- * runs on node.js with mongodb
--- db.js
+++ db.js
@@ -1,96 +1,28 @@
 
-
+require('joose');
 var mongoose = require('mongoose');
-var hashlib = require('hashlib');
+var Schema = mongoose.Schema, ObjectId = Schema.ObjectId;
 
-var Schema = mongoose.Schema, ObjectId = mongoose.Types.ObjectId;
-
-var User = new Schema({
+var user = new Schema({
+	id: ObjectId,
 	username: String,
-	password: {type:String, set: function(inp){ return hashlib.sha256(inp);}},
-	email: {type:String, set: function(inp){ return inp.toLowerCase();}},
-	registered: {type: Date, default: Date.now},
-	count: {type:Number, default: 0}
+	password: String,
+	registered: Date
 });
 
-var RawData = new Schema({
+var data = new Schema({
+	type: String,
+	owner: String,
 	content: String
 });
 
-var Revision = new Schema({
-	dataid: Schema.ObjectId,
-	date: {type: Date, default: Date.now},
-	prev: {type: Schema.ObjectId, default: null}
+Class('DataProvider',{
+	methods: {
+		authorizeUser: function(user) {},
+		getData: function(id,rev) {},
+		setData: function(id,newcontent) {}
+		 }
 });
 
-var Data = new Schema({
-	id: String,
-	type: String,
-	owner: String,
-	head: Schema.ObjectId,
-	revisions: [Revision],
-});
-
-
-mongoose.model('user',User);
-mongoose.model('data',Data);
-mongoose.model('rawdata',RawData);
-mongoose.model('revision',Revision);
-
-
-var DataProvider = function(){
-		User = mongoose.model('user');
-		Data  = mongoose.model('data');	 
-		RawData = mongoose.model('rawdata');
-};
-
-DataProvider.prototype = {
-	registerUser: function(param,cb) {
-		var user = new User(param);		
-		user.save();
-		console.log(user);
-		cb();
-	},
-	authorizeUser: function(param,cb) {
-		User.findOne({'username': param.username, 'password': hashlib.sha256(param.password)},function(err,res){
-			console.log("login:",param.username,param.password);
-			if(err)
-				console.log(err);
-				
-			cb(err,res);
-		});
-	},
-	getData: function(id,rev,cb) { 
-		Data.findOne({'id': id},function(err,res) {
-			if(err) 
-				return next(new Error('Could not load Document'));
-				
-			if(rev) //specific revision
-					cb("not implemented");//RawData.findById();
-			else // HEAD 
-			{
-				console.log("get: " + res.head);
-				RawData.findById(res.revisions.id(res.head).dataid,function(err,res){
-					cb(res);
-				});
-			}
-		});		
-	},
-	setData: function(id,newcontent) {},
-	addData: function(param) {
-		var data = new RawData();
-		data.content = param.content;
-		data.save();
-		
-		var ins = new Data();
-		ins.type = param.type;
-		ins.id = hashlib.sha512(new Date() + app.set('servername')); // TODO: + username, +usercount
-		ins.owner = param.owner;
-		ins.revisions.push({dataid: data._id});
-		ins.head = ins.revisions[0]._id;
-		ins.save();
-		console.log("new data: "+ins._id);
-	}
-};
 
 exports.DataProvider = DataProvider;
