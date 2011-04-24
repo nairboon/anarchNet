fdcf5287e9ec3d972660112297000e7b73d02402
nairboon <remo.hertig@bluewin.ch>
2011-04-24 17:17:15 +0200

    user authentication, basic data access/revision

--- app.js
+++ app.js
@@ -3,17 +3,29 @@
  * Module dependencies.
  */
 
-var express = require('express');
-var mongoose = require('mongoose');
-var MongoStore = require('express-session-mongo');
-require('express-resource');
+var express = require('express'),
+	mongoose = require('mongoose'),
+	MongoStore = require('connect-mongo'),
+	resource = require('express-resource'),
+	form = require('express-form'),
+	filter = form.filter,
+	validate = form.validate;
 
+require('express-namespace');
 var DataProvider = require('./db.js').DataProvider;
-var dp = new DataProvider();
 	
 var app = module.exports = express.createServer();
 
 // Configuration
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+  app.set('db-url', 'mongodb://localhost/test');
+  app.set('servername','rbose.org');
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
@@ -21,29 +33,28 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.cookieParser());
-  //app.use(express.session({ secret: 'your secret here', store: new MongoStore({db: 'test'}) }));
+  app.use(express.session({ secret: 'your secret here', store: new MongoStore({db: 'test'}) }));
   app.use(express.compiler({ src: __dirname + '/public', enable: ['less'] }));
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-  app.set('db-url', 'mongodb://localhost/test');
-});
 
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
-});
 
 var db = mongoose.connect(app.set('db-url'));
+var dp = new DataProvider();
 
 // Routes
-
 app.get('/', function(req, res){
   res.render('index', {
-    title: 'Express'
+    title: 'Parannus'
   });
+});
+
+
+app.get('/users', function(req, res){
+    // do something else
+res.send("hallo");
 });
 
 app.get('/app/:id', function(req, res){
@@ -56,16 +67,79 @@ app.get('/file/:id/:rev', function(req, res){
   res.send(dp.getData(req.params.id,req.params.rev).content)
 });
 
-app.get('/data/:id/:rev?', function(req, res,next){
-	next();
-  //res.send(dp.getData(req.params.id,req.params.rev));
+app.get('/data/:id?/:rev?', function(req, res,next){
+	var id = req.params.id;
+	if(id)
+		dp.getData(id,req.params.rev,function(data){
+			res.send(data);
+		});
+	else
+		next();
 });
 
-app.get('/data',function(req,res) {
-	res.send("HHH");
+app.get('/adddata',function(req,res) {
+	dp.addData({type:"xx", owner: "ll",content:"hallo world",id:"p"});
+	res.send("ok");
 });
 
 //app.resource("edit",require("./editor"));
+
+app.namespace('/auth', function() {
+	app.get('register',function(req,res){
+		res.render('auth/register', {
+		    title: 'Express'
+		  });
+	});
+	app.post('register',
+		form(
+			filter("username").trim(),
+			validate("username").required(),
+			validate("password").required(),
+			validate("email").required().isEmail()
+		),
+		function(req,res,next){
+			if(!req.form.isValid) {
+				console.log(req.form.errors);
+			}
+			else {
+				dp.registerUser(req.form, function(error) {
+					if(error)
+						console.log(error);
+						
+					res.send("reg successful");
+				});
+			}
+	});
+	app.get('login',function(req,res){
+		if(req.session.auth)
+			res.redirect("/");
+		res.render('auth/login', {
+		    title: 'Express'
+		  });
+	});
+	app.post('login',form(
+		validate("username").required(),
+		validate("password").required()
+		),
+		function(req,res){
+			dp.authorizeUser(req.form,function(error,r){
+				if(!r)
+					res.send("login failed");
+				else {
+					req.session.auth = true;
+					req.session.user = r;
+				res.send("authenticated");
+			}
+			});
+		
+	});
+	app.get('logout',function(req,res){
+		req.session.dextroy();
+		res.render('index', {
+		    title: 'logedout'
+		  });
+	});
+});
 
 if (!module.parent) {
   app.listen(3000);
