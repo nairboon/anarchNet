234983cd17d4eddf8781c089a548d4eb9e80431c
nairboon <remo.hertig@bluewin.ch>
2011-07-23 20:15:42 +0200

    login application (sample)

---
+++ repo/user/client.js
@@ -0,0 +1,23 @@
+
+function check()
+{
+	$('#location').html(location.hostname);
+	if(location.protocol === "https://")
+		$('#ssl-yes').show();
+	else 
+		$('#ssl-no').show();
+
+}
+
+
+$(document).ready(function(){
+	check();
+});
+
+function login() {
+	
+}
+
+function register() {
+
+}
