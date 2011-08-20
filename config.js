50b53049de4bd4f2805101b03e2157b82d94f2aa
nairboon <remo.hertig@bluewin.ch>
2011-08-20 17:57:43 +0200

    notepad test app

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
 	,filestore: './filestore'
 	,ssl_key: './ssl/privatekey.pem'
 	,ssl_cert: './ssl/certificate.pem'
-	,use_ssl: true
+	,use_ssl: false
 	,defaultmaster: '5f4d7ea22bcad14ee049907bd261d21f6dd1a3a877b4070a8f08b31d6ef93f0c'
 	,defaultrepo: '4e1b7d12f3b18a2520000025'
 };
