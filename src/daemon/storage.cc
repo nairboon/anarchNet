6ccd3843d0b48ae90400dece0227ef651c938eea
Remo Hertig <remo.hertig@bluewin.ch>
2012-07-11 21:47:35 +0200

    storage class

---
+++ src/daemon/storage.cc
@@ -0,0 +1,226 @@
+/*
+ * Copyright (C) 2012 Remo Hertig
+ * https://launchpad.net/anarchNet
+ *
+ * This file is part of anarchNet.
+ *
+ * anarchNet is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * anarchNet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with anarchNet.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <boost/thread.hpp>
+#include <boost/bind.hpp>
+#include <boost/foreach.hpp>
+#include "storage.h"
+#include "module_manager.h"
+#include "config_manager.h"
+#include "crypto.h"
+#include <fusekit/daemon.h>
+#include "logger.h"
+
+namespace an {
+
+
+ smart_file get(std::string fn)
+ {
+    return StorageManager::instance().open(fn);
+ }
+bool StorageManager::init(const std::string& mnt_point) {
+	  _daemon = &fusekit::daemon<an_directory>::instance();
+	    _file_cache = boost::shared_ptr<file_cache_type>(new file_cache_type(boost::bind(&StorageManager::load_file,this,_1),512));
+
+    boost::shared_ptr<boost::thread> fuse = boost::shared_ptr<boost::thread>(new boost::thread(boost::bind(&StorageManager::run,this)));
+    LOG(INFO) << "starting storage system, blocksize: " << BLOCKSIZE;
+    //_daemon->instance().root().mknod("test");
+	    return true;
+	}
+void StorageManager::run() {
+	   struct fuse_args args = FUSE_ARGS_INIT(0, NULL);
+
+	  fuse_opt_add_arg(&args, "anDaemon");
+         fuse_opt_add_arg(&args, "mnt");
+	  fuse_opt_add_arg(&args, "-f");
+
+     LOG(INFO) << args.argv[0];
+     _daemon->run(args.argc,args.argv);
+	}
+	smart_file StorageManager::open(fid_t id)
+	{
+	 return (*_file_cache)(id);
+	}
+	
+	     smart_file StorageManager::load_file(fid_t id)
+	     {
+	       smart_file res = smart_file(new file);
+	       fid_t db_id = crypto::Hash("DataMap"+id);
+	       an::KV_ResPtr kres;
+	       if(ModuleManager::instance().kv_get(db_id,kres))
+	       {
+		 BOOST_FOREACH(const an::KV_ResMap::value_type& entry, *kres)
+		 {
+		   if(entry.second.size() == AN_ID_LENGTH+2) // +2 DH magic byte
+		    if(entry.second[AN_ID_LENGTH] == 'D' && entry.second[AN_ID_LENGTH+1] == 'H') //its an old file
+		    {
+		      fid_t blockid = entry.second.substr(0,AN_ID_LENGTH);
+		      smart_block header1;
+		      if(!ModuleManager::instance().get_block(blockid,header1))
+		      {
+			LOG(ERROR) << "loading block:" << blockid;
+			throw 1;
+		      }
+		      if(header1.get()->size < sizeof(block_header))
+		      {
+			LOG(ERROR) << "header too small:" << header1.get()->size;
+			throw 1;
+		      }
+		      block_header raw_header;
+		      memcpy(&raw_header,header1.get()->data,sizeof(block_header));
+		      
+		      int blocks_needed = ceil(raw_header.size / ConfigManager::instance().blocksize());
+		      int blocks_per_file = ceil( (ConfigManager::instance().blocksize() - sizeof(block_header)) / AN_ID_LENGTH);
+		      if(blocks_needed > blocks_per_file) 
+		      {
+			LOG(ERROR) << "needs multi headers: " << blocks_per_file << ":" << blocks_needed;
+			throw 1;
+		      }
+		      res->size = raw_header.size;
+		      int offset = sizeof(block_header);
+		      for(int i = 0;i< blocks_needed;i++)
+		      {
+			char hash[AN_ID_LENGTH+1];
+			memcpy(&hash,header1.get()->data+offset,AN_ID_LENGTH);
+			hash[AN_ID_LENGTH] = '\0';
+			res->block_ids.push_back(fid_t(hash));
+			smart_block subblock;
+			  if(!ModuleManager::instance().get_block(res->block_ids.back(),subblock))
+			  {
+			    LOG(ERROR) << "loading block:" << blockid;
+			    throw 1;
+			  }
+			  res->data_blocks.push_back(subblock);
+		      }
+		    }
+		 }
+	       }
+	      return res;
+	     }
+	     bool file::add_block(smart_block b)
+	     {
+	       data_blocks.push_back(b);
+	       size+=b->size;
+	       LOG(INFO) << "added block:" << b->size;
+	       _current_block = b;
+	       return true;
+	     }
+	     bool file::store() // write new version of masterfile to disk
+	     {
+	     }
+	     
+	     std::ostream& operator<<( std::ostream& os, const an_file& f ){
+	       
+	      /* int b =f.sf->block_for_offset(f.sf->read_offset);
+	       long relative_offset = f.sf->read_offset - (b*ConfigManager::instance().blocksize());
+	       LOG(INFO) << "b: " << b << " ro: " << relative_offset;
+	       for(int i = b;i<f.sf->data_blocks.size();i++)
+	       {
+		 os.write(b->data,b->size - relative_offset);
+	       }
+	       */
+	      BOOST_FOREACH(smart_block b, f.sf->data_blocks)
+	       os.write(b->data,b->size);
+	       
+	       LOG(INFO) << "read req: " << f.fn;
+	     }
+	     
+	     std::istream& operator>>( std::istream& is, an_file& f ){
+	       std::string buff;
+	       long t_read = 0;
+	       while(is.good()) {
+		 
+		 is >> buff;
+		 int read = buff.size();
+		 t_read += read;
+		 LOG(DEBUG) << "read: " << read << " : " << t_read << " : " << buff;
+		 
+		 if(t_read >= BLOCKSIZE)
+		 {
+		   f.sf->write(buff.c_str(),t_read);
+		   LOG(DEBUG) << "sent to file: " << t_read;
+		   buff.clear();
+		   t_read = 0;
+		 }
+	       }
+	       // write remaining bytes
+	       if(t_read > 0)
+	       {
+		  f.sf->write(buff.c_str(),t_read);
+		   LOG(DEBUG) << "sent to file: " << t_read;
+	       }
+		 /*
+		 LOG(INFO) << "write req: " << f.fn;
+		 char *buff = new char[BLOCKSIZE+1];
+		 is.read(buff,BLOCKSIZE);
+		 int read = is.gcount();
+		 // FIXME data > blocksize
+		 if(read > 0)
+		 {
+		   LOG(DEBUG) << "wrote: " << buff << " :" << read;
+		   if(f.sf->data_blocks.size() > 1)
+		    memcpy(f.sf->data_blocks.front()->data,buff, BLOCKSIZE);
+		     else
+		   f.sf->add_block(smart_block(new block(buff,read)));
+		 }
+		 else
+		   LOG(ERROR)<< "stream bad" << read;
+	       }
+	       else
+		 LOG(ERROR)<< "stream bad" << f.fn;*/
+	     
+	     }
+	     
+	         int file::block_for_offset(long offset)
+		 {
+		   
+		   return ceil(offset / ConfigManager::instance().blocksize());
+
+		 }
+		 
+		     bool file::write(const char *data, long size)
+		     {
+		       LOG(DEBUG) << "file::write: " << size;
+		       if(data_blocks.size() == 0) // new file, no blocks
+		       {
+			 if(size <= BLOCKSIZE)
+			   add_block(smart_block(new block(data,size)));
+			 else {
+			   long rsize = BLOCKSIZE; 
+			   int i = 0;
+			   while(size > 0) {
+			      if(size < BLOCKSIZE)
+				rsize = size;
+			     add_block(smart_block(new block( data+i,rsize)));
+			     i += rsize;
+			     size -= rsize;  
+			  }
+			  _block_offset = rsize;
+			 }
+		       }
+		       else // existing file
+		       {
+			 LOG(DEBUG) << "FILE EXISTS";
+		       }
+		     }
+}
---
+++ src/daemon/storage.h
@@ -0,0 +1,135 @@
+/*
+ * Copyright (C) 2012 Remo Hertig
+ * https://launchpad.net/anarchNet
+ *
+ * This file is part of anarchNet.
+ *
+ * anarchNet is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * anarchNet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with anarchNet.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <vector>
+#include <boost/smart_ptr.hpp>
+#include <boost/concept_check.hpp>
+#include "singleton.h"
+#include "lru_cache.h"
+#include "anarchNet.h"
+#include "logger.h"
+
+#define FUSE_USE_VERSION 27
+#include <fusekit/daemon.h>
+#include <fusekit/default_directory.h>
+#include <fusekit/stream_object_file.h>
+#include <fusekit/new_creator.h>
+
+#ifndef SRC_DAEMON_STORAGE_H_
+#define SRC_DAEMON_STORAGE_H_
+
+
+#define BLOCKSIZE ConfigManager::instance().blocksize()
+#define ID_SIZE 512
+namespace an {
+  typedef std::string fid_t;
+  
+  class an_file;
+  
+  class block
+  {
+  public:
+    block(char *p,uint s) : data(p),size(s) { }
+    block(const char *p,uint s) : size(s) { data = new char[size]; memcpy(data,p,size); }
+    ~block() { delete[] data; }
+    fid_t hash();
+    char * data;
+    uint size;
+  };
+  typedef boost::shared_ptr<block> smart_block;
+  
+  class block_request {
+  };  
+  
+  struct block_header
+  {
+    unsigned long size;
+    char next[AN_ID_LENGTH];
+  };
+  
+  class file {
+  public:
+    file() : size(0), read_offset(0), write_offset(0) {}
+    bool add_block(smart_block b);
+    bool store();
+    fid_t id;
+    int size;
+    long read_offset;
+    long write_offset;
+    int block_for_offset(long);
+    bool write(const char *data, long size);
+    std::vector<fid_t> block_ids;
+    std::vector<smart_block> data_blocks;
+    std::vector<smart_block> header_blocks;
+  private:
+    long _block_offset;
+    smart_block _current_block;
+    
+  };
+  typedef boost::shared_ptr<file> smart_file;
+  class file_request {
+  };
+  
+  class StorageManager;
+  smart_file get(std::string fn);
+  struct an_file : public fusekit::iostream_object_file< an_file >::type{
+    an_file(std::string fn) 
+    : fusekit::iostream_object_file< an_file >::type(*this)
+    , fn(fn), sf(get(fn)){
+            LOG(DEBUG) << "open file:" << fn;
+    }
+    ~an_file() {
+      sf->store();
+      LOG(DEBUG) << "closed file:" << fn;
+    }
+    std::string fn;
+    smart_file sf;
+  };
+  
+  std::ostream& operator<<( std::ostream& os, const an_file& f );
+  std::istream& operator>>( std::istream& is, an_file& f );
+  
+  
+  struct an_directory 
+  : public fusekit::default_directory<
+  fusekit::directory_factory< fusekit::new_creator< an_directory > >,
+  fusekit::file_factory< fusekit::new_creator< an_file > >
+  >::type {
+    an_directory(){
+      
+    }
+  };
+  
+  
+  class StorageManager : public Singleton<StorageManager> {
+    friend class Singleton<StorageManager>;
+  public:
+    bool init(const std::string&);
+    void run();
+    smart_file open(fid_t id);
+    smart_file load_file(fid_t id);
+  private:
+    fusekit::daemon<an_directory> *_daemon;
+    typedef lru_cache_using_boost_set<std::string,an::smart_file>::type  file_cache_type;
+    
+    boost::shared_ptr<file_cache_type> _file_cache;
+  };
+}
+
+#endif
